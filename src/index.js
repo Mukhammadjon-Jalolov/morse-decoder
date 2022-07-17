44c067b1b85b9c0c5979ff5632d7f819d4f531e8
Muhammadjon <mjalolov1305@gmail.com>
2022-07-17 14:33:00 +0500

    completed the task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let code = expr;
+    let numofletters = expr.length / 10;
+    let arrofcodes = [];
+    let resstr = '';
+    let result = '';
+
+    for (let i = 0; i < numofletters; i++){
+        arrofcodes.push(code.substr(0, 10));
+        code = code.replace(code.substr(0, 10), "");
+    }
+
+    const compare = (one) => {
+        let temp = '';
+
+        for(x in MORSE_TABLE){
+            let n = '';
+            n = x.replace(/\./g, "10").replace(/-/g, "11");    
+            n.length < 10 ? n = "0".repeat(10 - n.length) + n : n = n;
+
+            if(one === n){
+                temp = MORSE_TABLE[x];
+            }
+        }
+        return temp;
+    }
+
+    arrofcodes.forEach((item) => {
+        let harf = compare(item);
+        result += harf;
+    })
+    
+    return result;
 }
 
 module.exports = {
